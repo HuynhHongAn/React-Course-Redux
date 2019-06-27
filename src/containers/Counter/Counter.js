cdd168f485c5b905b01335f549c3c230df299ccf
HuynhHongAn <huynhhongan2908@gmail.com>
2019-06-27 17:56:24 +0700

    Add global const for actions

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -59,12 +60,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-        onAddCounter: (amount) => dispatch({ amount: amount, type: 'ADD' }),
-        onSubtractCounter: (amount) => dispatch({ amount: amount, type: 'SUBTRACT' }),
-        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-        onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', id: id })
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAddCounter: (amount) => dispatch({ amount: amount, type: actionTypes.ADD }),
+        onSubtractCounter: (amount) => dispatch({ amount: amount, type: actionTypes.SUBTRACT }),
+        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, id: id })
     }
 }
 
