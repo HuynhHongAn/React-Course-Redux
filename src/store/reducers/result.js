9b78a09848b79d0328979a7ad497c98e16e9e8a5
HuynhHongAn <huynhhongan2908@gmail.com>
2019-06-28 15:30:16 +0700

    Using unility function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
   results: []
@@ -7,10 +8,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: action.counter})
-      }
+      return updateObject(state, {results: state.results.concat({id: new Date(), value: action.counter})})
     case actionTypes.DELETE_RESULT:
       //1)
       // const id = 2;
@@ -21,10 +19,7 @@ const reducer = (state = initialState, action) => {
       const updatedArray = state.results.filter(result => {
         return result.id !== action.id;
       })
-      return {
-        ...state,
-        results: updatedArray
-      }
+      return updateObject(state, {results: updatedArray})
   }
 
   return state;
